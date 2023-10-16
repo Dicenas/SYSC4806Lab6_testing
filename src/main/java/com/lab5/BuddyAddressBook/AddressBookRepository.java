1cdcded6d73bd9ae35e88b034a2da9791eeb6ef7
trong0dn <55768917+trong0dn@users.noreply.github.com>
2023-10-16 01:36:25 -0400

    Added testing and upgrade ui front-end

--- src/main/java/com/lab5/BuddyAddressBook/AddressBookRepository.java
+++ src/main/java/com/lab5/BuddyAddressBook/AddressBookRepository.java
@@ -2,7 +2,6 @@ package com.lab5.BuddyAddressBook;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.lang.NonNull;
-import org.springframework.lang.NonNullApi;
 
 import java.util.List;
 
